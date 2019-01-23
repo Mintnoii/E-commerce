9575090ab8fec559808d0940adea0c0ec7d01fb9
W-Qing <scorpio971028@gmail.com>
2019-01-23 13:30:11 +0800

    :memo:  exit.vue

---
+++ pages/exit.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class=""/>
+</template>
+
+<script>
+export default {
+  layout: 'blank',
+  // 利用中间件来实现退出功能，路由到达退出页面后会自动执行这个动作
+  middleware: async (ctx) => {
+    let {status,data}=await ctx.$axios.get('/users/exit')
+    if(status===200&&data&&data.code===0){
+      window.location.href='/'
+    }
+  }
+}
+</script>
