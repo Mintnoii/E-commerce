6bac65ad7fa51f825943fd2052e10f94c79b82d3
W-Qing <scorpio971028@gmail.com>
2019-02-12 21:13:47 +0800

    :memo: products and crumbs

---
+++ components/products/crumbs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="m-crumbs">
+    <!--面包屑组件-->
+    <el-breadcrumb separator=">">
+      <!--直接从vuex中获取数据(去掉城市中的‘市’)，不用再发送异步请求-->
+      <el-breadcrumb-item :to="{ path: '/' }">{{ $store.state.geo.position.city.replace('市','') }}美团</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">{{ $store.state.geo.position.city.replace('市','') }}{{ decodeURIComponent(keyword) }}</a></el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    // 从外部传递进来关键词
+    keyword: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
