27e10f816f1ed5fb466e6f0b224d9900feaaed6e
W-Qing <scorpio971028@gmail.com>
2019-05-05 23:24:39 +0800

    update

--- components/public/footer/index.vue
+++ components/public/footer/index.vue
@@ -7,42 +7,42 @@
             <dt>用户帮助</dt>
             <dd><a
               rel="nofollow"
-              href="http://www.meituan.com/help/selfservice"
+              href="http://www.weixin.com/help/selfservice"
               target="_blank">申请退款</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://www.meituan.com/help/selfservice?tab=2"
+              href="http://www.weixin.com/help/selfservice?tab=2"
               target="_blank">查看券密码</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://www.meituan.com/help/faq"
+              href="http://www.weixin.com/help/faq"
               target="_blank">常见问题</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://www.meituan.com/about/terms"
+              href="http://www.weixin.com/about/terms"
               target="_blank">用户协议</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://www.meituan.com/about/privacy"
+              href="http://www.weixin.com/about/privacy"
               target="_blank">隐私政策</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://www.meituan.com/about/anticheat"
+              href="http://www.weixin.com/about/anticheat"
               target="_blank">反诈骗公告</a></dd>
           </dl>
           <dl>
             <dt>服务</dt>
             <dd><a
-              href="http://waimai.meituan.com/"
+              href="http://waimai.weixin.com/"
               target="_blank">外卖</a></dd>
             <dd><a
-              href="http://hotel.meituan.com/"
+              href="http://hotel.weixin.com/"
               target="_blank">酒店</a></dd>
             <dd><a
               href="http://maoyan.com/"
               target="_blank">猫眼电影</a></dd>
             <dd><a
-              href="https://peisong.meituan.com/"
+              href="https://peisong.weixin.com/"
               target="_blank">配送</a></dd>
             <dd><a
               href="https://www.mtyun.com/"
@@ -51,7 +51,7 @@
               href="http://www.dianping.com/"
               target="_blank">大众点评</a></dd>
             <dd><a
-              href="https://phoenix.meituan.com/"
+              href="https://phoenix.weixin.com/"
               target="_blank">榛果民宿</a></dd>
           </dl>
         </div>
@@ -60,28 +60,28 @@
             <dt>商家合作</dt>
             <dd><a
               rel="nofollow"
-              href="https://ecom.meituan.com/bizsettle/settle?utm_source=mt_C_my"
+              href="https://ecom.weixin.com/bizsettle/settle?utm_source=mt_C_my"
               target="_blank">美食商家入驻(非外卖)</a></dd>
             <dd><a
-              href="https://kd.meituan.com/"
+              href="https://kd.weixin.com/"
               target="_blank">外卖开店申请</a></dd>
             <dd><a
-              href="http://shouyin.meituan.com?utm_source=inner&amp;utm_medium=mtpc"
+              href="http://shouyin.weixin.com?utm_source=inner&amp;utm_medium=mtpc"
               target="_blank">收银官网</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://page.peisong.meituan.com/apply/join"
+              href="http://page.peisong.weixin.com/apply/join"
               target="_blank">外卖配送加盟申请</a></dd>
             <dd><a
-              href="https://xue.meituan.com/?from=mtpc"
+              href="https://xue.weixin.com/?from=mtpc"
               target="_blank">点评餐饮学院</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://ruzhu.meituan.com/ruzhu/index.html"
+              href="http://ruzhu.weixin.com/ruzhu/index.html"
               target="_blank">酒店商家入驻</a></dd>
             <dd><a
               rel="nofollow"
-              href="https://i.meituan.com/awp/h5/trip/business/cooperation/index.html?client=pc"
+              href="https://i.weixin.com/awp/h5/trip/business/cooperation/index.html?client=pc"
               target="_blank">境内度假商家入驻</a></dd>
             <dd><a
               rel="nofollow"
@@ -89,30 +89,30 @@
               target="_blank">综合商家入驻</a></dd>
             <dd><a
               rel="nofollow"
-              href="https://phoenix.meituan.com/about/"
+              href="https://phoenix.weixin.com/about/"
               target="_blank">榛果民宿房东商家入驻</a></dd>
             <dd><a
-              href="http://pc.meituan.com/?activity_code=mtpcdb"
+              href="http://pc.weixin.com/?activity_code=mtpcdb"
               target="_blank">商家开票申请</a></dd>
             <dd><a
               rel="nofollow"
-              href="https://store.meituan.com/?utm_source=17"
+              href="https://store.weixin.com/?utm_source=17"
               target="_blank">点评智能收银机</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://developer.meituan.com/"
+              href="http://developer.weixin.com/"
               target="_blank">聚宝盆餐饮开放平台</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://smartpay.meituan.com/"
+              href="http://smartpay.weixin.com/"
               target="_blank">智能支付</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://paidui.meituan.com/"
+              href="http://paidui.weixin.com/"
               target="_blank">排队</a></dd>
             <dd><a
               rel="nofollow"
-              href="https://i.meituan.com/awp/hfe/block/6c4bec785dce/11188/index.html"
+              href="https://i.weixin.com/awp/hfe/block/6c4bec785dce/11188/index.html"
               target="_blank">快驴进货商家合作</a></dd>
           </dl>
         </div>
@@ -120,11 +120,11 @@
           <dl><dt>代理商加盟</dt>
             <dd><a
               rel="nofollow"
-              href="https://i.meituan.com/awp/hfe/block/4544/index.html"
+              href="https://i.weixin.com/awp/hfe/block/4544/index.html"
               target="_blank">智能支付渠道加盟商招募</a></dd>
             <dd><a
               rel="nofollow"
-              href="https://daili.meituan.com/?comeFrom=mtwebBusinesscoopd"
+              href="https://daili.weixin.com/?comeFrom=mtwebBusinesscoopd"
               target="_blank">到店餐饮代理商招募</a></dd>
             <dd><a
               rel="nofollow"
@@ -132,29 +132,29 @@
               target="_blank">非餐饮代理商招募</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://union.meituan.com/"
+              href="http://union.weixin.com/"
               target="_blank">联盟</a></dd>
             <dd><a
               rel="nofollow"
-              href="https://store.meituan.com/distribution/broadcast"
+              href="https://store.weixin.com/distribution/broadcast"
               target="_blank">收银招募线上分销商</a></dd>
             <dd><a
               rel="nofollow"
-              href="https://agent.meituan.com/zhaoshang?entry=5"
+              href="https://agent.weixin.com/zhaoshang?entry=5"
               target="_blank">点评5S服务商招募</a></dd>
           </dl>
           <dl>
             <dt>行业规范</dt>
             <dd><a
               rel="nofollow"
-              href="http://www.meituan.com/about/saferule"
+              href="http://www.weixin.com/about/saferule"
               target="_blank">点评餐饮安全管理办法</a></dd>
           </dl>
           <dl>
             <dt>廉正举报</dt>
             <dd><a
               rel="nofollow"
-              href="mailto:lianzheng@meituan.com"
+              href="mailto:lianzheng@weixin.com"
               target="_self">廉政邮箱</a></dd>
           </dl>
         </div>
@@ -163,26 +163,26 @@
             <dt>关注</dt>
             <dd><a
               rel="nofollow"
-              href="http://weibo.com/meituan"
+              href="http://weibo.com/weixin"
               target="_blank">新浪微博</a></dd>
           </dl>
           <dl>
             <dt>公司信息</dt>
             <dd><a
               rel="nofollow"
-              href="http://zhaopin.meituan.com/"
+              href="http://zhaopin.weixin.com/"
               target="_blank">加入我们</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://www.meituan.com/about/law"
+              href="http://www.weixin.com/about/law"
               target="_blank">法律声明</a></dd>
             <dd><a
               rel="nofollow"
-              href="http://www.meituan.com/about/bizlicense"
+              href="http://www.weixin.com/about/bizlicense"
               target="_blank">商户服务协议</a></dd>
             <dd><a
               rel="nofollow"
-              href="https://dpapp-appeal.meituan.com/#/shopCreditRegulationPC"
+              href="https://dpapp-appeal.weixin.com/#/shopCreditRegulationPC"
               target="_blank">商户诚信公约及管理办法</a></dd>
           </dl>
         </div>
@@ -220,23 +220,23 @@
               target="_blank">4006018900</a></dd>
             <dd>举报邮箱： <a
               rel="nofollow"
-              href="mailto:tousujubao@meituan.com"
-              target="_self">tousujubao@meituan.com</a></dd>
+              href="mailto:tousujubao@weixin.com"
+              target="_self">tousujubao@weixin.com</a></dd>
           </dl>
           <dl>
             <dt><a
               rel="nofollow"
-              href="https://ecom.meituan.com/bizsettle/settle/merchantsSettle"
+              href="https://ecom.weixin.com/bizsettle/settle/merchantsSettle"
               target="_blank">商家自助入驻入口</a></dt>
           </dl>
         </div>
       </div>
       <div class="footer-copyright clearfix">
         <div class="footer-copyright-left">
-          <p>©网团购 meituan.com <a href="http://www.beianbeian.com/beianxinxi/283f39a9-4c00-427a-97ef-3c7a9e1e0af1.html">京ICP证070791号</a> <a href="http://www.miitbeian.gov.cn/">京ICP备10211739号</a> <a
-            href="http://www.meituan.com/about/rules"
+          <p>©网团购 weixin.com <a href="http://www.beianbeian.com/beianxinxi/283f39a9-4c00-427a-97ef-3c7a9e1e0af1.html">京ICP证070791号</a> <a href="http://www.miitbeian.gov.cn/">京ICP备10211739号</a> <a
+            href="http://www.weixin.com/about/rules"
             target="_blank">电子公告服务规则</a></p><p><a
-              href="http://i.meituan.com/brunch/default/right"
+              href="http://i.weixin.com/brunch/default/right"
               target="_blank">广播电视节目制作经营许可证（京）字第03889号</a></p>
         </div>
         <div class="footer-copyright-right">
@@ -245,7 +245,7 @@
             target="_blank">京公网安备11010502025545号</a>
           <div class="footer-copyright-cert"><a
             class="sp-ft sp-ft--record"
-            href="http://www.meituan.com/about/openinfo"
+            href="http://www.weixin.com/about/openinfo"
             title="备案信息"
             target="_blank">备案信息</a><a
               class="sp-ft sp-ft--knet"
